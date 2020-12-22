61bd1f66979fca23dd688384901d65a173295a30
sml0399 <sml0399@naver.com>
2020-12-23 03:43:28 +0900

    min_kmap_solver.cpp updated: new function names defined and modified flow of main function

--- min_kmap_solver.cpp
+++ min_kmap_solver.cpp
@@ -7,14 +7,18 @@
 using namespace std;
 
 
-bool is_valid(string formula){ /*check whether input form is valid. Only alphabets, +, ' are allowed*/
+bool is_valid(string formula){ /*check whether input form is valid. Only alphabets, +, ', paranthesis are allowed*/
 	for( int i = 0; i < formula.length(); i++ ){
 		const char * character=&formula[i];
 		// FIX: indexing returns char, not std::string. Need to convert back to std::string if you really want to use compare.
-		if(( (strncmp(character,"A",1) < 0 ) || ( ( strncmp(character,"Z",1) > 0 ) && ( strncmp(character,"a",1) < 0 ) ) || ( strncmp(character,"z",1) > 0 ) ) && ( strncmp(character,"'",1) !=0 ) && ( strncmp(character,"+",1) != 0 ) ){
+		if(( (strncmp(character,"A",1) < 0 ) || ( ( strncmp(character,"Z",1) > 0 ) && ( strncmp(character,"a",1) < 0 ) ) || ( strncmp(character,"z",1) > 0 ) ) && ( strncmp(character,"'",1) !=0 ) && ( strncmp(character,"+",1) != 0 ) && ( strncmp(character,"(",1) !=0 ) && ( strncmp(character,")",1) !=0 )){
 			return false;
 		}
 	}
+	
+	/*TODO: paranthesis matching test*/
+
+
 	return true;
 }
 
@@ -25,32 +29,51 @@ vector<char> get_eight_alpha(string formula){/*get alphabets in the input form. 
 	return characters;
 }
 
-string convert_to_regular_alpha(string formula){ /*convert user inputs using fixed alphabets(a,b,c,d,e,f,g,h)*/
+string convert_to_regular_alpha(string formula, vector<char> characters){ /*convert user inputs using fixed alphabets(a,b,c,d,e,f,g,h)*/
+	string result="abc";
 
 
 
-
-	return "";
+	return result;
 }
-/*
+
+string convert_to_original_alpha(string formula, vector<char> characters){ /*convert result to user defined alphabets*/
+	string result="abc";
+
+
+	return result;
+}
+
+string formula_expansion(string formula){
+	string result="abc";
+
+
+	return result;
+}
+
 string to_SOP(string formula){
+	string result="abc";
 
 
-
-	return ;
+	return result;
 }
 
 
-*/
+
 
 
 int main(void){ /*temporary main. This part will be removed after other functionalities are implemented*/
 	string formula = "";
+	vector<char> characters={};
 	cout << "Enter the formula: ";
 	cin >> formula;
 	if( !is_valid(formula) ){ cout << "Error in the formula" << endl; return -1; }
-
-	cout << formula << endl;
+	characters=get_eight_alpha(formula);
+	string regular_formula=convert_to_regular_alpha(formula, characters);
+	regular_formula=formula_expansion(regular_formula);
+	string solution=to_SOP(regular_formula);
+	
+	cout << solution << endl;
 	return 0;
 
 
