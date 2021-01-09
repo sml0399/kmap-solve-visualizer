554db8b62d661f2bc6a3cc8d1fc0d431baeaad23
KangSK-KAIST <steelyk7@gmail.com>
2021-01-09 19:42:13 +0900

    Fixed main.cpp and moved error codes to functions

--- main.cpp
+++ main.cpp
@@ -6,14 +6,31 @@ using namespace std;
 
 int main(void) {
 	string formula = "";
-	vector<char> characters={};
+	vector<char> char_set = {};
+	
 	cout << "Enter the formula: ";
-	getline(cin,formula);
-	formula=preprocess(formula);
-	if( !is_valid(formula) ){ cerr << "Error in the formula" << endl; return -1; }
-	characters=get_eight_alpha(formula);
-	if((characters.size()>8)||(characters.size()<1)){cerr<< "Error in the formula. We allow eight different alphabet characters at maximum and requires at least one alphabet"<<endl;return -1;}
-	string regular_formula=convert_to_regular_alpha(formula, characters);
+	getline(cin, formula);
+
+	formula = preprocess(formula);
+	// cout << formula << endl;
+
+	if (!is_valid(formula)) {
+		cerr << "Error in the formula" << endl;
+		return -1;
+	}
+
+	try {
+		char_set = get_eight_alpha(formula);
+	} catch (const char* msg) {
+		return -1;
+	}
+
+	for (char c : char_set)
+		cout << c << endl;
+
+	string regular_formula = convert_to_regular_alpha(formula, char_set);
+
+	cout << regular_formula << endl;
 
 /*	regular_formula=formula_expansion(regular_formula);
 	string solution=to_SOP(regular_formula);  
